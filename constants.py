253701902c0cf687c381a64d132bff97860f54d7
alek-pc <101040127+alek-pc@users.noreply.github.com>
2022-11-10 01:11:15 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,2 +1,10 @@
 MONTHS_NAMES = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май',
                 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+
+DARK_THEME_COLORS = {'text_color': '#ffffff', 'background_color': '#000000'}
+
+LIGHT_THEME_COLORS = {'text_color': '#000000', 'background_color': '#ffffff'}
+
+STANDARD_CALENDAR_COLORS = {'workday_color': '#0055ff', 'weekend_color': '#ff0000', 'common_holiday_color': '#ff0000',
+                            'personal_holiday_color': '#ff0000', 'additional_weekend_color': '#ff0000',
+                            'notice_color': '#55ff00'}
